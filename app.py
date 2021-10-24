9659f819c5ea68dbec09e8e965db94f1e33e719f
cobobc <ciaranob4@gmail.com>
2021-10-24 19:29:01 +0000

    Add about content to index.html

--- app.py
+++ app.py
@@ -25,6 +25,11 @@ def recipes():
     return render_template("recipes.html")
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
